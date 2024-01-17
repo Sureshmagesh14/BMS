553b12332912be8833462b44ae7372bf51f8cc29
VijayMust <vijay018>
2024-01-17 11:56:24 +0530

    changes

---
+++ resources/views/admin/layout/header.blade.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Dashboard | Drezon - Responsive Bootstrap 4 Admin Dashboard</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Themesbrand" name="author" />
+
+        @section('adminside-favicon')
+            <!-- App favicon -->
+            <link rel="shortcut icon" href="{{ asset('public/assets/images/favicon.ico') }}">
+        @stop
+        @section('adminside-css')
+            <!-- Bootstrap Css -->
+            <link href="{{ asset('public/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+            <!-- Icons Css -->
+            <link href="{{ asset('public/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+            <!-- App Css-->
+            <link href="{{ asset('public/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+        @stop
+    </head>
+
+    <body data-sidebar="dark">
+        <!-- <body data-layout="horizontal" data-topbar="colored"> -->
+
+            <!-- Begin page -->
+            <div id="layout-wrapper">
+
+                <header id="page-topbar">
+                    <div class="navbar-header">
+                        <div class="d-flex">
+                            <!-- LOGO -->
+                            <div class="navbar-brand-box">
+                                <a href="index.html" class="logo logo-dark">
+                                    <span class="logo-sm">
+                                        <img src="{{ asset('public/assets/images/logo-dark-sm.png') }}" alt="" height="22">
+                                    </span>
+                                    <span class="logo-lg">
+                                        <img src="{{ asset('public/assets/images/logo-dark.png') }}" alt="" height="18">
+                                    </span>
+                                </a>
+                
+                                <a href="index.html" class="logo logo-light">
+                                    <span class="logo-sm">
+                                        <img src="{{ asset('public/assets/images/logo-light-sm.png') }}" alt="" height="22">
+                                    </span>
+                                    <span class="logo-lg">
+                                        <img src="{{ asset('public/assets/images/logo-light.png') }}" alt="" height="18">
+                                    </span>
+                                </a>
+                            </div>
---
+++ resources/views/admin/layout/vertical_side_menu.blade.php
@@ -0,0 +1,153 @@
+<!-- ========== Left Sidebar Start ========== -->
+<div class="vertical-menu">
+
+    <!-- LOGO -->
+    <div class="navbar-brand-box">
+        <a href="index.html" class="logo logo-dark">
+            <span class="logo-sm">
+                <img src="{{ asset('public/assets/images/logo-dark-sm.png') }}" alt="" height="22">
+            </span>
+            <span class="logo-lg">
+                <img src="{{ asset('public/assets/images/logo-dark.png') }}" alt="" height="18">
+            </span>
+        </a>
+
+        <a href="index.html" class="logo logo-light">
+            <span class="logo-sm">
+                <img src="{{ asset('public/assets/images/logo-light-sm.png') }}" alt="" height="22">
+            </span>
+            <span class="logo-lg">
+                <img src="{{ asset('public/assets/images/logo-light.png') }}" alt="" height="18">
+            </span>
+        </a>
+    </div>
+
+    <button type="button" class="btn btn-sm px-3 font-size-16 header-item waves-effect vertical-menu-btn">
+        <i class="fa fa-fw fa-bars"></i>
+    </button>
+
+    <div data-simplebar class="sidebar-menu-scroll">
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+                <li class="menu-title" key="t-menu">Menu</li>
+
+                <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <i class="icon nav-icon" data-feather="home"></i>
+                        <span class="menu-item" key="t-dashboards">Dashboards</span>
+                    </a>
+                  
+                </li>
+
+               
+                <li class="menu-title" key="t-apps">Admin</li>
+
+                <li>
+                    <a href="calendar.html" class="waves-effect">
+                        <i class="icon nav-icon" data-feather="calendar"></i>
+                        <span class="menu-item" key="t-calendar">Users</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="chat.html" class=" waves-effect">
+                        <i class="icon nav-icon" data-feather="message-square"></i>
+                        <span class="menu-item" key="t-chat">Internal Reports</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="chat.html" class=" waves-effect">
+                        <i class="icon nav-icon" data-feather="message-square"></i>
+                        <span class="menu-item" key="t-chat">Cash Outs</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="chat.html" class=" waves-effect">
+                        <i class="icon nav-icon" data-feather="message-square"></i>
+                        <span class="menu-item" key="t-chat">Action Events</span>
+                    </a>
+                </li>
+              
+
+            
+
+                <li class="menu-title" key="t-pages">Database</li>
+
+                <li>
+                    <a href="calendar.html" class="waves-effect">
+                        <i class="icon nav-icon" data-feather="calendar"></i>
+                        <span class="menu-item" key="t-calendar">Projects</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="chat.html" class=" waves-effect">
+                        <i class="icon nav-icon" data-feather="message-square"></i>
+                        <span class="menu-item" key="t-chat">Respondents</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="chat.html" class=" waves-effect">
+                        <i class="icon nav-icon" data-feather="message-square"></i>
+                        <span class="menu-item" key="t-chat">Panels</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="chat.html" class=" waves-effect">
+                        <i class="icon nav-icon" data-feather="message-square"></i>
+                        <span class="menu-item" key="t-chat">Rewards</span>
+                    </a>
+                </li>
+
+                <li class="menu-title">Settings</li>
+
+                <li>
+                    <a href="calendar.html" class="waves-effect">
+                        <i class="icon nav-icon" data-feather="calendar"></i>
+                        <span class="menu-item" key="t-calendar">Profile Groups</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="chat.html" class=" waves-effect">
+                        <i class="icon nav-icon" data-feather="message-square"></i>
+                        <span class="menu-item" key="t-chat">Banks</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="chat.html" class=" waves-effect">
+                        <i class="icon nav-icon" data-feather="message-square"></i>
+                        <span class="menu-item" key="t-chat">Charities</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="chat.html" class=" waves-effect">
+                        <i class="icon nav-icon" data-feather="message-square"></i>
+                        <span class="menu-item" key="t-chat">Cellular Networks
+                        </span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="chat.html" class=" waves-effect">
+                        <i class="icon nav-icon" data-feather="message-square"></i>
+                        <span class="menu-item" key="t-chat">Contents
+                        </span>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+</div>
+<!-- Left Sidebar End -->
