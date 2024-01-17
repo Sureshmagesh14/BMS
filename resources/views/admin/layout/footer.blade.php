989f7797ef8bd28edb400cb25a21dfcb7d5c26eb
hemanathans1 <hemanathan@pabrai.com>
2024-01-17 14:33:55 +0530

    bms

---
+++ resources/views/admin/layout/footer.blade.php
@@ -0,0 +1,94 @@
+    <footer class="footer">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-6">
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script> © Drezon.
+                </div>
+                <div class="col-sm-6">
+                    <div class="text-sm-right d-none d-sm-block">
+                        Crafted with <i class="mdi mdi-heart text-danger"></i> by <a href="https://themesbrand.com/"
+                            target="_blank" class="text-reset">Themesbrand</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+<!-- end main content-->
+
+</div>
+<!-- END layout-wrapper -->
+@section('adminside-theme')
+    <!-- Right Sidebar -->
+    <div class="right-bar">
+        <div data-simplebar class="h-100">
+            <div class="rightbar-title px-3 py-4">
+                <a href="javascript:void(0);" class="right-bar-toggle float-right">
+                    <i class="mdi mdi-close noti-icon"></i>
+                </a>
+                <h5 class="m-0">Settings</h5>
+            </div>
+
+            <!-- Settings -->
+            <hr class="mt-0" />
+            <h6 class="text-center mb-0">Choose Layouts</h6>
+
+            <div class="p-4">
+                <div class="mb-2">
+                    <img src="{{ asset('public/assets/images/layouts/layout-1.jpg') }}" class="img-fluid img-thumbnail" alt="">
+                </div>
+                <div class="custom-control custom-switch mb-3">
+                    <input type="checkbox" class="custom-control-input theme-choice" id="light-mode-switch" checked />
+                    <label class="custom-control-label" for="light-mode-switch">Light Mode</label>
+                </div>
+
+                <div class="mb-2">
+                    <img src="{{ asset('public/assets/images/layouts/layout-2.jpg') }}" class="img-fluid img-thumbnail" alt="">
+                </div>
+                <div class="custom-control custom-switch mb-3">
+                    <input type="checkbox" class="custom-control-input theme-choice" id="dark-mode-switch" data-bsStyle="{{ asset('public/assets/css/bootstrap-dark.min.css') }}" data-appStyle="{{ asset('assets/css/app-dark.min.css') }}" />
+                    <label class="custom-control-label" for="dark-mode-switch">Dark Mode</label>
+                </div>
+
+                <div class="mb-2">
+                    <img src="{{ asset('public/assets/images/layouts/layout-3.jpg') }}" class="img-fluid img-thumbnail" alt="">
+                </div>
+                <div class="custom-control custom-switch mb-5">
+                    <input type="checkbox" class="custom-control-input theme-choice" id="rtl-mode-switch" data-appStyle="{{ asset('public/assets/css/app-rtl.min.css') }}" />
+                    <label class="custom-control-label" for="rtl-mode-switch">RTL Mode</label>
+                </div>
+            </div>
+        </div> <!-- end slimscroll-menu-->
+    </div>
+    <!-- /Right-bar -->
+@stop
+
+<!-- Right bar overlay-->
+<div class="rightbar-overlay"></div>
+
+
+    <!-- JAVASCRIPT -->
+    <script src="{{ asset('public/assets/libs/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('public/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('public/assets/libs/metismenu/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('public/assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('public/assets/libs/node-waves/waves.min.js') }}"></script>
+    <script src="{{ asset('public/assets/libs/waypoints/lib/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('public/assets/libs/jquery.counterup/jquery.counterup.min.js') }}"></script>
+    <script src="{{ asset('public/assets/libs/feather-icons/feather.min.js') }}"></script>
+
+    <!-- apexcharts -->
+    <script src="{{ asset('public/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+
+    <script src="{{ asset('public/assets/js/pages/dashboard.init.js') }}"></script>
+
+    <script src="{{ asset('public/assets/js/app.js') }}"></script>
+
+
+</body>
+
+
+<!-- Mirrored from themesbrand.com/drezon/layouts/vertical/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 06 Jul 2022 05:49:13 GMT -->
+</html>
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/','CustomAuthController@index')->name('login');
+
+Route::get('dashboard','CustomAuthController@dashboard')->name('dashboard');
+Route::any('custom-login','CustomAuthController@customLogin')->name('login.custom');
+Route::get('register','CustomAuthController@registration')->name('register');
+Route::post('custom-registration','CustomAuthController@customRegistration')->name('register.custom');
+Route::get('signout','CustomAuthController@signOut')->name('signout');
