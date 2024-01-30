38d1f2369083707c102c7b7f41836681f708ddd5
hemanathans1 <hemanathan@pabrai.com>
2024-01-30 14:41:53 +0530

    bms

--- app/Http/Controllers/RespondentsController.php
+++ app/Http/Controllers/RespondentsController.php
@@ -32,67 +32,56 @@ class RespondentsController extends Controller
     public function get_all_respondents(Request $request) {
 		
         try {
-            if ($request->ajax()) {
+                if ($request->ajax()) {
 
                 $token = csrf_token();
-            
-                
+
                 $all_datas = DB::table('respondents')
                 ->orderby("id","desc")
                 ->limit(10)
                 ->get();
-        
-            
-            // $all_datas = Respondents::select('respondents.*','projects.name as uname')
-            // ->join('respondent_profiles', 'respondents.id', '=', 'respondent_profiles.respondent_id') 
-            // ->orderby("id","desc")
-            // ->get();
 
-            $all_datas = DB::table('respondents')
-            ->orderby("id","desc")
-            ->limit(10)
-            ->get();
-    
-            
-            return Datatables::of($all_datas)
-            ->addColumn('name', function ($all_data) {
-                return $all_data->name;
-            })  
-            ->addColumn('surname', function ($all_data) {
-                return $all_data->surname;
-            })  
-            ->addColumn('mobile', function ($all_data) {
-                return $all_data->mobile;
-            })  
-            ->addColumn('whatsapp', function ($all_data) {
-                return $all_data->whatsapp;
-            })  
-            ->addColumn('email', function ($all_data) {
-                return $all_data->email;
-            }) 
-            ->addColumn('age', function ($all_data) {
-                return $all_data->date_of_birth;
-            })
-            ->addColumn('gender', function ($all_data) {
-                return '-';
-            })
-            ->addColumn('race', function ($all_data) {
-                return '-';
-            })
-            ->addColumn('status', function ($all_data) {
-                return '-';
-            })
-            ->addColumn('profile_completion', function ($all_data) {
-                return '-';
-            })
-            ->addColumn('inactive_until', function ($all_data) {
-                return '-';
-            })
-            ->addColumn('opeted_in', function ($all_data) {
-                return '-';
-            })
-            ->rawColumns(['name','surname','mobile','whatsapp','email','age','gender','race','status','profile_completion','inactive_until','opeted_in'])      
-            ->make(true);
+
+                return Datatables::of($all_datas)
+                ->addColumn('name', function ($all_data) {
+                    return $all_data->name;
+                })  
+                ->addColumn('surname', function ($all_data) {
+                    return $all_data->surname;
+                })  
+                ->addColumn('mobile', function ($all_data) {
+                    return $all_data->mobile;
+                })  
+                ->addColumn('whatsapp', function ($all_data) {
+                    return $all_data->whatsapp;
+                })  
+                ->addColumn('email', function ($all_data) {
+                    return $all_data->email;
+                }) 
+                ->addColumn('age', function ($all_data) {
+                    return $all_data->date_of_birth;
+                })
+                ->addColumn('gender', function ($all_data) {
+                    return '-';
+                })
+                ->addColumn('race', function ($all_data) {
+                    return '-';
+                })
+                ->addColumn('status', function ($all_data) {
+                    return '-';
+                })
+                ->addColumn('profile_completion', function ($all_data) {
+                    return '-';
+                })
+                ->addColumn('inactive_until', function ($all_data) {
+                    return '-';
+                })
+                ->addColumn('opeted_in', function ($all_data) {
+                    return '-';
+                })
+                ->rawColumns(['name','surname','mobile','whatsapp','email','age','gender','race','status','profile_completion','inactive_until','opeted_in'])      
+                ->make(true);
+                }
         }
         catch (Exception $e) {
             throw new Exception($e->getMessage());
