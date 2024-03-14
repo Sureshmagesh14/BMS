554c2ec1e1302b958bd66604131d5ac150b6cb23
hemanathans1 <hemanathan@pabrai.com>
2024-03-14 16:01:00 +0530

    bms

--- app/Models/Respondents.php
+++ app/Models/Respondents.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class Respondents extends Authenticatable
+class Respondents extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
