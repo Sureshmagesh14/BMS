6bd5eaa36e18cf740cdc3e076e4620fc375702f1
hemanathans1 <hemanathan@pabrai.com>
2024-03-27 16:50:30 +0530

    bms

--- resources/views/user/user-surveys.blade.php
+++ resources/views/user/user-surveys.blade.php
@@ -11,7 +11,7 @@ $catname = array(1=>'Basic',2=>'Essential',3=>'Extended');
             <img class="img-fluid" src="{{ asset('assets/images/Characters-01.png') }}" alt=""/>
           </div>
           <div class="col-md-8">
-            <h3 class="text-center">{{ $catname[$data->type_id] }}</h3>
+            <h3 class="text-center">{{ $catname[$data->type_id] }} </h3>
             <div class="bg-white text-center py-5  d-flex flex-column">
               <!-- <div class="completed-items"><span>4/10</span></div> -->
                 <div class="w-100">
@@ -28,6 +28,7 @@ $catname = array(1=>'Basic',2=>'Essential',3=>'Extended');
                  
                   
                   <form>
+                    <input type="hidden" name="cat" id="cat" value="questions{{ request()->up }}">
                   <div class="question-container"></div>
                   </form>
 
@@ -60,116 +61,76 @@ $catname = array(1=>'Basic',2=>'Essential',3=>'Extended');
 $(document).ready(function() {
 
     $('#nav_surveys').addClass('active');
+    
 
-    var questions = [
-   {   text: 'How much wood could a woodchuck chuck if a woodchuck could chuck wood?', 
-         id: '1', 
-       type: 'single-select', 
-    options: ["37", "112", "42", "100"]},
-  {   text: 'What has a face and two hands but no arms or legs?', 
-         id: '2', 
-       type: 'single-select', 
-    options: ["a clock", "a dog", "a couch", "a giraffe"]},
-  {   text: 'What gets wetter and wetter the more it dries?', 
-         id: '3', 
-       type: 'single-select', 
-    options: ["a car", "a towel", "a plane", "a television"]},
-  {   text: 'You throw away the outside and cook the inside. Then you eat the outside and throw away the inside. What did you eat?', 
-         id: '4', 
-       type: 'single-select', 
-    options: ["a cucumber", "an ear of corn", "a watermelon", "cheese"]},
-  {   text: 'What goes up and down the stairs without moving?', 
-         id: '5', 
-       type: 'single-select', 
-    options: ["a toddler", "an arrow", "towels", "a rug"]},
-  {   text: 'What can you catch but not throw?', 
-         id: '6', 
-       type: 'single-select', 
-    options: ["a couch", "a cold", "a puppy", "a baseball"]},
-  {   text: 'I can run but not walk. Wherever I go, thought follows close behind. What am I?', 
-         id: '7', 
-       type: 'single-select', 
-    options: ["a doctor", "a pineapple", "a nose", "pimples"]},
-  {   text: 'What\'s black and white and red all over?', 
-         id: '8', 
-       type: 'single-select', 
-    options: ["an embarrased skunk", "a turtle", "a giraffe", "a dog"]},
-  {   text: 'What goes around the world but stays in a corner?', 
-         id: '9', 
-       type: 'single-select', 
-    options: ["a stamp", "coffee", "a dog", "plants"]},
-  {   text: 'I have holes in my top and bottom, my left and right, and in the middle. But I still hold water. What am I?', 
-         id: '10', 
-       type: 'single-select', 
-    options: ["a turtle", "a toenail", "a sponge", "a towel"]},
-  {   text: 'Give me food, and I will live; give me water, and I will die. What am I?', 
-         id: '11', 
-       type: 'single-select', 
-    options: ["a cold", "fire", "couch", "a microwave"]},
-  {   text: 'The man who invented it doesn\'t want it. The man who bought it doesn\'t need it. The man who needs it doesn\'t know it. What is it?', 
-         id: '12', 
-       type: 'single-select', 
-    options: ["a pillow", "a coffee grinder", "a coffin", "a computer"]},
-  {   text: 'I run over fields and woods all day. Under the bed at night I sit not alone. My tongue hangs out, up and to the rear, awaiting to be filled in the morning. What am I?', 
-         id: '13', 
-       type: 'single-select', 
-    options: ["a shoe", "a dog", "a nail", "a wallet"]},
-  {   text: 'Throw it off the highest building, and I\'ll not break. But put me in the ocean, and I will. What am I?', 
-         id: '14', 
-       type: 'single-select', 
-    options: ["a penny", "a car", "a shell", "a tissue"]},
-  {   text: 'What can run but never walks, has a mouth but never talks, has a head but never weeps, has a bed but never sleeps?', 
-         id: '15', 
-       type: 'single-select', 
-    options: ["a river", "a lake", "an ocean", "a car"]},
-  {   text: 'No sooner spoken than broken. What is it?', 
-         id: '16', 
-       type: 'single-select', 
-    options: ["silence", "onions", "paper", "cats"]},
-  {   text: 'A certain crime is punishable if attempted but not punishable if committed. What is it?', 
-         id: '17', 
-       type: 'single-select', 
-    options: ["smoking", "coup d'etat", "horseplaying", "swimming"]},
-  {   text: 'You use a knife to slice my head and weep beside me when I am dead. What am I?', 
-         id: '18', 
-       type: 'single-select', 
-    options: ["an onion", "toilet paper", "toenails", "an ottoman"]},
-  {   text: 'I\'m the part of the bird that\'s not in the sky. I can swim in the ocean and yet remain dry. What am I?', 
-         id: '19', 
-       type: 'single-select', 
-    options: ["a beak", "a foot", "a feather", "a shadow"]},
-  {   text: 'I am mother and father, but never birth or nurse. I\'m rarely still, but I never wander. What am I?', 
-         id: '20', 
-       type: 'single-select', 
-    options: ["headphones", "kittens", "a tree", "a hippy"]}
-];
+});
+    var questions2 = [
+        {   
+        text: 'How much wood could a woodchuck chuck if a woodchuck could chuck wood?', 
+        id: '1', 
+        type: 'multi-select', 
+        options: ["37", "112", "42", "100", "112", "42", "100", "112", "42", "100"]},
 
-var portionOfWindowSizeForQuestions = 0.9;
+        {   
+        text: 'What has a face and two hands but no arms or legs?', 
+        id: '2', 
+        type: 'single-select', 
+        options: ["a clock", "a dog", "a couch", "a giraffe"]},
+        {     
+        text: 'hat gets wetter and wetter the more it dries?', 
+        id: '3', 
+        type: 'single-select', 
+        options: ["a car", "a towel", "a plane", "a television"]}
+  ];
+
+  var questions1 = [
+        {   
+       
+        text: 'In addition to the less frequent, higher paying research, you can now also do lots of short online surveys, and all that money adds up quickly.', 
+
+        id: '1', 
+        type: 'no-option', 
+        options: [""]},
+
+        {   
+
+        text: 'Would you like to do 1-2hr online or face-to-face research paying up to R1200 (max 2 per year)?<br>- Focus Group Discussions- Interviews<br>- Mystery Shops<br>- Multiday online communities', 
+        id: '2', 
+        type: 'single-select', 
+        options: ["Yes", "No"]}
+  ];
+
+var portionOfWindowSizeForQuestions = 0.2;
 var firstQuestionDisplayed = 0;
 var lastQuestionDisplayed = 0;
 
 $(document).ready(function(){
-   
-  questions.forEach(function(question) {
-    generateQuestionElement( question );
-  });
+  var curr_questions = $('#cat').val();
+
+
+    questions<?php echo request()->up; ?>.forEach(function(question) {
+      generateQuestionElement( question );
+    });
+
+
   
   $('#backBtn').click(function() {
     if ( !$('#backBtn').hasClass('disabled') ) {
-      showPreviousQuestionSet();
+      showPreviousQuestionSet(curr_questions);
     }
   });
   
   $('#nextBtn').click(function() {
     if ( $('#nextBtn').text() === 'Next' ) {
-      showNextQuestionSet();
+      showNextQuestionSet(curr_questions);
     } else {
       // call submitting stuff here.  
+      alert("done");
     }
     
   });
   
-  showNextQuestionSet();
+  showNextQuestionSet(curr_questions);
      
 });
 
@@ -192,6 +153,13 @@ function generateQuestionElement(question) {
         '<label class="radio"><input type="radio" value="' + option + '" name="' + questionId + '"/>' + option + '</label>');
     });
   }
+  if ( question.type === 'multi-select' ) {
+    questionElement.addClass('multi-select');
+    question.options.forEach(function(option) {
+      questionAnswerElement.append(
+        '<label class="checkobx"><input type="checkbox" value="' + option + '" name="' + questionId + '"/>' + option + '</label>');
+    });
+  }
   questionElement.hide();
 }
 
@@ -203,7 +171,7 @@ function hideAllQuestions() {
 }
 
 
-function showNextQuestionSet() {
+function showNextQuestionSet(org1) {
   hideAllQuestions();
   var finished = false;
   firstQuestionDisplayed = ++lastQuestionDisplayed;
@@ -213,7 +181,7 @@ function showNextQuestionSet() {
     var nextElement = $('.question-container > div.question:nth-child(' + (lastQuestionDisplayed + 1) + ')');
     nextElement.show();
 
-    if ( lastQuestionDisplayed >= questions.length || $('.question-container').height() > $(window).height() * portionOfWindowSizeForQuestions ) {
+    if ( lastQuestionDisplayed >= org1.length || $('.question-container').height() > $(window).height() * portionOfWindowSizeForQuestions ) {
       nextElement.hide();
       finished = true;
     } else {
@@ -221,11 +189,11 @@ function showNextQuestionSet() {
     }
   } while( !finished );
   
-  doButtonStates();
+  doButtonStates(org1);
 }
 
 
-function showPreviousQuestionSet() {
+function showPreviousQuestionSet(org1) {
   hideAllQuestions();
   var finished = false;
   lastQuestionDisplayed = --firstQuestionDisplayed;
@@ -244,12 +212,12 @@ function showPreviousQuestionSet() {
     
   } while( !finished );
   
-  doButtonStates();
+  doButtonStates(org1);
   
 }
 
 
-function doButtonStates() {
+function doButtonStates(org1) {
   
   if ( firstQuestionDisplayed == 1 ) {
     $('#backBtn').addClass('disabled');  
@@ -257,12 +225,12 @@ function doButtonStates() {
     $('#backBtn').removeClass('disabled');
   }
     
-  if ( lastQuestionDisplayed == questions.length ) {
+  if ( lastQuestionDisplayed == org1.length ) {
     $('#nextBtn').text('Finish');    
   } else if ( $('#nextBtn').text() === 'Finish' ) {
     $('#nextBtn').text('Next');  
   }
 }
 
-});
+
 </script>
