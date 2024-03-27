3a8b3e9237d20be8d597e5e4a84ae29627f4d1be
hemanathans1 <hemanathan@pabrai.com>
2024-03-27 15:43:53 +0530

    bms

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -168,13 +168,16 @@ class WelcomeController extends Controller
     public function user_surveys(Request $request){
         try {
             
+            $up_id = $request->up;
             $resp_id =Session::get('resp_id');
             $resp_name =Session::get('resp_name');
             
+            $data = Groups::where('id', $up_id)->first();
+
             // if($request->user()->profile_completion_id==0){
             //     return view('user.update-profile');
             // }else{
-                return view('user.user-surveys');
+                return view('user.user-surveys',compact('data'));
            // }
            
         }
