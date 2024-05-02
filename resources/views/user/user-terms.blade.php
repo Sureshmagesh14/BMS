ed3de071b0048d99cb7636d40dcbf806d3f329fc
VijayMust <vijay@pabrai.com>
2024-05-02 17:56:03 +0530

    changes

--- resources/views/user/user-terms.blade.php
+++ resources/views/user/user-terms.blade.php
@@ -1,13 +1,39 @@
-@include('front.layout.header')
+@include('user.layout.header-2')
 
 <section class="vh-md-100 bg-white">
-      <div class="container">
+    <div class="container">
         <div class="row justify-content-center py-5 m-auto">
-        <p>
-            {{$data->data}}
-        </p>
+            <div calss = "container">
+              @if(strlen($data->data) > 300)
+              <p>{{substr($data->data, 0, 33440) }}<span
+                id="dots">...</span>
+                @else
+                <span
+                        id="more">{{substr($data->data, 0, 33440) }}</span>
+              </p>
+              @endif
+           
+                <button onclick="myFunction()" id="myBtn">Read more</button>
+            </div>
         </div>
-      </div>
+    </div>
 </section>
 
-@include('front.layout.footer')
+@include('user.layout.footer')
+<script>
+    function myFunction() {
+        var dots = document.getElementById("dots");
+        var moreText = document.getElementById("more");
+        var btnText = document.getElementById("myBtn");
+
+        if (dots.style.display === "none") {
+            dots.style.display = "inline";
+            btnText.innerHTML = "Read more";
+            moreText.style.display = "none";
+        } else {
+            dots.style.display = "none";
+            btnText.innerHTML = "Read less";
+            moreText.style.display = "inline";
+        }
+    }
+</script>
