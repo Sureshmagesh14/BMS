5352cadb2f5722ed9083a3e9826c90ddeedbffa4
VijayMust <vijay@pabrai.com>
2024-06-21 12:01:42 +0530

    cahhghjgfghfg

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -83,6 +83,7 @@
 
             <div class="row">
 <<<<<<< HEAD
+<<<<<<< HEAD
                 <div class="col-12 text-right">
                     <div class="btn-group dropdown-filter">
                         <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
@@ -139,30 +140,67 @@
                         <div class="card-body">
 =======
 
+=======
+>>>>>>> e0408ee (changes)
                 <div class="col-12 text-right">
-                    <select id="year">
-                        @for ($i = date('Y'); $i >= date('Y') - 10; $i--)
-                            <option value="{{ $i }}">{{ $i }}</option>
-                        @endfor
-                    </select>
-
-                    <select name="month" id="month" size='1'>
-                        @for ($i = 0; $i < 12; $i++)
-                            @php
-                                $time = strtotime(sprintf('%d months', $i));
-                                $label = date('F', $time);
-                                $value = date('n', $time);
-                            @endphp
-                            <option value='{{ $value }}'>{{ $label }}</option>
-                        @endfor
-                    </select>
-                    <button type="submit" class="btn btn-default btn-primary" onclick="banks_table();">Submit</button>
+                    <div class="btn-group dropdown-filter">
+                        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" aria-labelledby="filter" role="presentation" class="fill-current text-80">
+                                <path fill-rule="nonzero" d="M.293 5.707A1 1 0 0 1 0 4.999V1A1 1 0 0 1 1 0h18a1 1 0 0 1 1 1v4a1 1 0 0 1-.293.707L13 12.413v2.585a1 1 0 0 1-.293.708l-4 4c-.63.629-1.707.183-1.707-.708v-6.585L.293 5.707zM2 2v2.585l6.707 6.707a1 1 0 0 1 .293.707v4.585l2-2V12a1 1 0 0 1 .293-.707L18 4.585V2H2z"></path>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="6" viewBox="0 0 10 6" class="ml-2">
+                                <path fill="var(--90)" d="M8.292893.292893c.390525-.390524 1.023689-.390524 1.414214 0 .390524.390525.390524 1.023689 0 1.414214l-4 4c-.390525.390524-1.023689.390524-1.414214 0l-4-4c-.390524-.390525-.390524-1.023689 0-1.414214.390525-.390524 1.023689-.390524 1.414214 0L5 3.585786 8.292893.292893z"></path>
+                            </svg>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <div class="card p-2" style="max-height: 300px; overflow-y: auto;">
+                                <ul class="list-unstyled">
+                                    <li class="mb-2">
+                                        <h5>Type Filter</h5>
+                                        <select name="type_filter" id="type_filter" class="form-control" onchange="cashout_type(this)">
+                                            <option value="">Select Type</option>
+                                            @for ($i = date('Y'); $i >= date('Y') - 10; $i--)
+                                                <option value="{{ $i }}">{{ $i }}</option>
+                                            @endfor
+                                        </select>
+                                    </li>
+                                    <li class="mb-2">
+                                        <h5>Status Filter</h5>
+                                        <select name="status_filter" id="status_filter" class="form-control" onchange="cashout_status(this)">
+                                            <option value="">Select Status</option>
+                                            @php
+                                                // Get current month and year
+                                                $currentMonth = date('n');
+                                                $currentYear = date('Y');
+                                        
+                                                $months = [];
+                                                for ($i = 0; $i < 12; $i++) {
+                                                    $timestamp = mktime(0, 0, 0, $currentMonth + $i, 1, $currentYear);
+                                                    $label = date('F', $timestamp);
+                                                    $value = date('n', $timestamp);
+                                                    $months[$value] = $label;
+                                                }
+                                                ksort($months);
+                                            @endphp
+                                            @foreach ($months as $value => $label)
+                                                <option value="{{ $value }}">{{ $label }}</option>
+                                            @endforeach
+                                        </select>
+                                        
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-12">
+                <div class="col-xl-12">
                     <div class="card">
                         <div class="card-body">
+<<<<<<< HEAD
 
 >>>>>>> 7fac792 (changes)
+=======
+>>>>>>> e0408ee (changes)
                             <table id="user_events" class="table dt-responsive nowrap w-100">
                                 <thead>
                                     <tr>
@@ -176,10 +214,13 @@
                                 <tbody></tbody>
                             </table>
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 
 >>>>>>> 7fac792 (changes)
+=======
+>>>>>>> e0408ee (changes)
                         </div>
                     </div>
                 </div>
