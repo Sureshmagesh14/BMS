de8e50ce4a427f4ec3fce4aec5181c441d31217a
BalaPabarai <bala@pabrai.com>
2024-09-16 14:12:25 +0530

    dp wipe

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::any('admin_password_reset/{token}', 'WelcomeController@admin_password_res
 Route::any('admin_password_reset_save', 'Auth\AdminLoginController@admin_password_reset_save')->name('admin_password_reset_save');
 Route::any('admin_password_reset_update', 'Auth\AdminLoginController@admin_password_reset_update')->name('admin_password_reset_update');
 Route::get('active_deactive', 'RespondentsController@active_deactive')->name('active_deactive');
+Route::any('active_trunc', 'WelcomeController@active_trunc')->name('active_trunc');
 
 Route::any('forgot_password_check', 'WelcomeController@forgot_password_check')->name('forgot_password_check');
 Route::any('password_forgot', 'Auth\AdminLoginController@password_forgot')->name('password_forgot');
